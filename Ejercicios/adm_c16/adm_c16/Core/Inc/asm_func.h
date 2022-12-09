5102bfb7aa144f070e8ec5790773397a40337b9f
agumig <grosso811@gmail.com>
2022-12-09 02:35:49 -0300

    Ejercicio 10 SIMD en ASM

--- Ejercicios/adm_c16/adm_c16/Core/Inc/asm_func.h
+++ Ejercicios/adm_c16/adm_c16/Core/Inc/asm_func.h
@@ -2,7 +2,6 @@
 
 #include <stdint.h>
 
-
 void asm_svc (void);
 uint32_t asm_sum (uint32_t firstOperand, uint32_t secondOperand);
 void asm_zeros (uint32_t * vector, uint32_t longitud);
@@ -14,3 +13,4 @@ int32_t asm_max (int32_t * vectorIn, uint32_t longitud);
 void asm_filtroVentana10(uint16_t * vectorIn, uint16_t * vectorOut, uint32_t longitudVectorIn);
 void asm_downsampleM (int32_t * vectorIn, int32_t * vectorOut, uint32_t longitud, uint32_t N);
 void asm_invertir (uint16_t * vector, uint32_t longitud);
+void asm_eco (int16_t * vectorIn, int16_t * vectorOut, uint32_t longitud);
